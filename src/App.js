80e7da3ac896e7594fd9b76cf4c077729c081268
la-livia <114690192+la-livia@users.noreply.github.com>
2023-01-26 23:54:39 +0100

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>React Weather App</h1>
+        <footer>
+          This project was coded by Lívia Laczová and is{" "}
+          <a
+            href="https://github.com/la-livia/react-shecodes-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced
+          </a>{" "}
+          on GitHub.
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
